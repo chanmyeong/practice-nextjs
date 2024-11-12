1f956e8aa6ed3d455cc3665c55f03aac898d87e6
김주언 <wndjs803@naver.com>
2024-11-12 14:00:44 +0900

    metadata

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "Home",
+};
+
 export default function Page() {
   return (
     <div>
@@ -8,3 +12,4 @@ export default function Page() {
 
 // 디렉토리 구조 자체가 url ex) /company
 // 만약 디레토리가 있어도 page.tsx가 없다면 유효한 페이지가 아님
+// (home) -> 괄호로 둘러싸여진 디렉토리는 url에 영향을 주지 않는다.
