c7b13efda0975d245dc3a7e4ecdd160f0dbf6976
김주언 <wndjs803@naver.com>
2024-11-13 16:50:14 +0900

    server side

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,24 +1,23 @@
-"use client";
+export const metadata = {
+  title: "Home",
+};
 
-import { useEffect, useState } from "react";
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-export default function Page() {
-  const [isLoading, setLoading] = useState(true);
-  const [movies, setMovies] = useState();
-  const getMovies = async () => {
-    const response = await fetch(
-      "https://nomad-movies.nomadcoders.workers.dev/movies"
-    );
-    const json = await response.json();
-    setMovies(json);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  return <div>{isLoading ? "Loading..." : JSON.stringify(movies)}</div>;
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 5000)); // 5초 지연
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
 }
 
 // 디렉토리 구조 자체가 url ex) /company
 // 만약 디레토리가 있어도 page.tsx가 없다면 유효한 페이지가 아님
 // (home) -> 괄호로 둘러싸여진 디렉토리는 url에 영향을 주지 않는다.
+
+// server component 사용시 fetch된 url 캐싱
