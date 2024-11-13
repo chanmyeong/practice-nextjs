73dddd06c10698833e1a3a2cd59c6295eb54ffb4
김주언 <wndjs803@naver.com>
2024-11-13 17:47:50 +0900

    parallel requests

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,10 +1,25 @@
-export default function MovieDetail({
+import { API_URL } from "../../../(home)/page";
+
+async function getMovie(id: string) {
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+
+async function getVideos(id: string) {
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
+}
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <h1>Movie {id}</h1>;
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+  return <h1>{movie.title}</h1>;
 }
 
 // []를 통해 동적 라우팅을 표시할 수 있다.
 // movies/123
+
+// Promise.all()는 자바스크립트에서 여러 비동기 작업을 동시에 실행하고, 모든 작업이 완료될 때까지 기다렸다가 결과를 배열 형태로 반환하는 함수입니다. 쉽게 말해, 여러 Promise를 모두 이행할 때까지 기다린 후, 그 결과를 한꺼번에 받아볼 수 있게 해줍니다.
